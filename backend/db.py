4c821c241f481802ea15d9e2972a4aef70c76d1c
Vlad-Shevkunov <vshevkunov@berkeley.edu>
2025-04-29 14:03:00 +0200

    introducing database URL env var

--- backend/db.py
+++ backend/db.py
@@ -4,12 +4,9 @@ import os
 from dotenv import load_dotenv
 load_dotenv()  # This loads the variables from .env
 
-
 def get_db_connection():
     return psycopg2.connect(
-        dbname=os.environ.get("DB_NAME"),
-        user=os.environ.get("DB_USER"),  # Use your actual username
-        password=os.environ.get("DB_PASSWORD"),  # Replace with your actual password
-        host=os.environ.get("HOST"),
+        dsn=os.environ["DATABASE_URL"],
+        sslmode=os.environ.get("PGSSLMODE", "require"),
         cursor_factory=RealDictCursor
     )
